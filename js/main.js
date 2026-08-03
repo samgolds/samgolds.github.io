2829fa767ee9d13ea381446bd5f964f1d23048b2
Sam Goldstein <samgolds@sas.upenn.edu>
2026-08-02 23:14:08 -0700

    updated website

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 function loadImage(id, targetId) {
   var el = document.getElementById(id);
+  if (!el) return;
   var targetEl = targetId ? document.getElementById(targetId) : el;
   var imageToLoad;
 
   if (el.dataset.image) {
-    imageToLoad - el.dataset.image;
-    // console.log("1st " + imageToLoad);
-  } else if (typeof el.currentSrc === undefined) {
+    imageToLoad = el.dataset.image;
+  } else if (typeof el.currentSrc === "undefined") {
     imageToLoad = el.src;
-    // console.log("2nd " + imageToLoad);
   } else {
     imageToLoad = el.currentSrc;
-    // console.log("3rd " + imageToLoad);
   }
-  console.log(imageToLoad);
+
   if (imageToLoad) {
     var img = new Image();
     img.src = imageToLoad;
@@ -24,6 +22,5 @@ function loadImage(id, targetId) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  loadImage("wallpaper"); 
   loadImage("pictureImage", "picture");
 });
